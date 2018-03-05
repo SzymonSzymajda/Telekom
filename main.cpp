4550d377105af3d5a101100ad2db4421c6267b15
SzymonSzymajda <szymonicho@gmail.com>
2018-03-05 22:07:31 +0100

    sprawdzania poprawnosci i porawa slow kodowych o N bitow kontrolnych i M-N bitow wiadomosci

--- main.cpp
+++ main.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  cout << "gitara siema, elo";
+
+  ifstream file;
+  file.open("dane.txt");
+  int rows, cols;
+  file >> rows >> cols;
+  int matrix[rows][cols];
+  for(int i=0; i<rows; i++)
+  {
+    for (int  j = 0; j < cols; j++) {
+      file >> matrix[i][j];
+    }
+  }
+  file.close();
+  //wczytaj slowo kodowe
+  int word[cols];
+  for (int i = 0; i < cols; i++) {
+    cin >> word[i];
+  }
+  //sprawdz poprawnosc
+  int error[rows];
+  bool isCorrect = true;
+  for (int i = 0; i < rows; i++) {
+    error[i] = 0;
+    for (int j = 0; j < cols; j++) {
+      error[i] += word[j] * matrix[i][j];
+    }
+    error[i] = error[i]%2;
+    if(error[i]==1) isCorrect = false;
+  }
+
+  if(isCorrect)
+  {
+    cout << "Slowo jest poprawne";
+    return 0;
+  }
+  else
+  {
+    cout << "Slowo nie jest poprawne";
+  }
+  //popraw slowo jesli niepoprawne
+  bool corrected = false;
+  for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++) {
+      if(error[i]!=matrix[i][j]) break;
+      if(i==rows-1)
+      {
+        word[j] = word[j] + 1;
+        word[j] = word[j]%2;
+        bool corrected = true;
+      }
+      if(corrected) break;
+    }
+  }
+  //wyswietl poprawione slowo
+  cout << "\n";
+  for (size_t i = 0; i < cols; i++) {
+    cout<<word[i]<<" ";
+  }
   return 0;
 }
