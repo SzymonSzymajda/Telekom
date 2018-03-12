bfa3eec3236f2317acd683e3bf2cd1e1e17044dc
SzymonSzymajda <szymonicho@gmail.com>
2018-03-12 13:00:15 +0100

    poprawa 2 bledow

--- Zadanie 1/main.cpp
+++ Zadanie 1/main.cpp
@@ -5,7 +5,16 @@
 
 using namespace std;
 
+//wczytywanie macierzy
+//kodowanie slowa
+//wczytanie slowa
+//sprawdznie poprawnosci
+
+
 void load_matrix(vector< vector<int> >&, int&, int&);
+bool isZero(vector< vector<int> >&, vector<int>&, vector<int>&, int&, int&);
+bool correct1(vector< vector<int> >&, vector<int>&, vector<int>&, int&, int&);
+bool correct2(vector< vector<int> >&, vector<int>&, vector<int>&, int&, int&);
 
 /*
 H[][]       - matrix    kontrolna
@@ -25,94 +34,80 @@ int main()
     load_matrix(matrix, rows, cols);
     cout << '\n' << rows << " x " << cols << endl;
 
-int word0[cols];
-for (int i = 0; i < cols; i++) {
+    int word0[cols];
+    for (int i = 0; i < cols; i++) {
     word0[i] = 0;
-}
+    }
+    int pBits = cols-rows;
 //wczytaj slowo
-cout << "\nWpisz slowo:\n";
-for (int i=0; i<cols-4; i++)
-    cin >> word0[i];
-cout << endl;
+    cout << "\nWpisz slowo:\n";
+    for (int i=0; i<pBits; i++)
+        cin >> word0[i];
+    cout << endl;
 
 //zakoduj slowo
-int pBits = cols-rows;
-for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < pBits; j++) {
-        word0[pBits+i] += word0[j] * matrix[i][j];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < pBits; j++) {
+            word0[pBits+i] += word0[j] * matrix[i][j];
+        }
+        word0[pBits+i] %= 2;
     }
-    word0[pBits+i] %= 2;
-}
 //wyswietl zakodowane slowo
-cout << "Zakodowane slowo: ";
-for (int i = 0; i < cols; i++) {
-    cout << word0[i] << " ";
-}
-cout << endl;
+    cout << "\nZakodowane slowo:       ";
+    for (int i = 0; i < cols; i++) {
+        cout << word0[i] << " ";
+    }
 
 // wczytaj zakodowane slowo
-    cout << "Podaj zakodowane slowo: ";
-    int word1[cols];
-    for (int i=0; i<cols; i++)
-        cin >> word1[i];
+    cout << "\nPodaj zakodowane slowo: ";
+    vector<int> word1;
+    for (int i=0, tmp; i<cols; i++)
+    {
+        cin >> tmp;
+        word1.push_back(tmp);
+    }
     cout << endl;
 
 // wektor bledow
-    int error[rows];
-    bool isCorrect = true;
-    for (int i=0; i<rows; i++)
-    {
-        error[i] = 0;
-        for (int j=0; j<cols; j++)
-            error[i] += word1[j] * matrix[i][j];
-        error[i] %= 2;
-        if(error[i] == 1)
-        {
-            isCorrect = false;
-        }
-    }
+    vector<int> error;
 
-    if(isCorrect)
+    if( isZero(matrix, word1, error, rows, cols) )
     {
         cout << "\nSlowo jest poprawne";
         return 0;
     }
-    cout << "\nSlowo jest niepoprawne";
+    cout << "\nSlowo jest niepoprawne"
+         << "\n                        ";
 
 // popraw slowo jesli niepoprawne
-    bool corrected = false;
-    for(int j=0; j<cols; j++)
+    if( correct1(matrix, word1, error, cols, rows) )
     {
-        for(int i=0; i<rows; i++)
+        for (int i=0; i<cols; i++)
         {
-            if(error[i] != matrix[i][j])
-                break;
-            if(i == rows-1)
-            {
-                word1[j] += 1;
-                word1[j] %= 2;
-                corrected = true;
-            }
-            if(corrected)
-                break;
+            cout<<word1[i]<<" ";
         }
     }
-
-// wyswietl poprawione slowo
-    cout << "\n";
-    for (int i=0; i<cols; i++)
+    else if( correct2(matrix, word1, error, cols, rows) )
     {
-        cout<<word1[i]<<" ";
+        for (int i=0; i<cols; i++)
+        {
+            cout<<word1[i]<<" ";
+        }
+    }
+    else
+    {
+        cout << "\nZa duzo bledow\n";
     }
 
     cout << "\n- - -S T O P- - -";
+
     return 0;
 }
 
 void load_matrix(vector< vector<int> >& matrix, int& rows, int& cols)
 {
     ifstream fin;
-    fin.open("dane.txt");
+    fin.open("dane1.txt");
     if(!fin)
     {
         cout << "\nNie mozna otworzyc pliku. To koniec";
@@ -133,4 +128,64 @@ void load_matrix(vector< vector<int> >& matrix, int& rows, int& cols)
     }
 
     fin.close();
+}
+
+bool isZero(vector< vector<int> >& matrix, vector<int>& word1, vector<int>& error, int& rows, int& cols)
+{
+    bool isCorrect = true;
+    for (int i=0; i<rows; i++)
+    {
+        error.push_back(0);
+        for (int j=0; j<cols; j++)
+            error[i] += word1[j] * matrix[i][j];
+        error[i] %= 2;
+        if(error[i] == 1)
+            isCorrect = false;
+    }
+    return isCorrect;
+}
+
+bool correct1(vector< vector<int> >& matrix, vector<int>& word1, vector<int>& error, int& cols, int& rows)
+{
+    for(int j=0; j<cols; j++)
+    {
+        for(int i=0; i<rows; i++)
+        {
+            if(error[i] != matrix[i][j])
+                break;
+            if(i == rows-1) // dopiero po sprawdzeniu ze cala kolumna jest rowna wektorowi bledow
+            {
+                word1[j] += 1;
+                word1[j] %= 2;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool correct2(vector< vector<int> >& matrix, vector<int>& word1, vector<int>& error, int& cols, int& rows)
+{
+    vector<int> temp(rows);
+    for(int i=0; i<cols; i++)
+    {
+        for(int j=i+1; j<cols; j++)
+        {
+            for(int k=0; k<rows; k++)
+            {
+                temp[k] = (matrix[k][i] + matrix[k][j]) % 2;
+                if(temp[k] != error[k])
+                    break;
+                if(k == rows - 1)
+                {
+                    word1[i] += 1;
+                    word1[j] += 1;
+                    word1[i] %= 2;
+                    word1[j] %= 2;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
